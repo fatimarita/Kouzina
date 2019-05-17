00ee363859264e200615f953aa43b5ea239c0131
fatima <fatima.elmajdoub@gmail.com>
2019-05-17 13:43:53 +0200

    la meme procedure pour les pages sucrées

--- src/Controller/SucreesController.php
+++ src/Controller/SucreesController.php
@@ -2,20 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Recette;
+use App\Repository\RecetteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 
 class SucreesController extends AbstractController
 {
     /**
      * @Route("/recettes sucrées", name="sucrees")
+     * @param RecetteRepository $repo
      * @return Response
      */
 
-    public function index(): Response
+    public function index(RecetteRepository $repo)
     {
-        return $this->render('sucrees/sucrees.html.twig');
+
+        $recettes = $repo->findAll();
+
+        return $this->render('sucrees/sucrees.html.twig', [
+            'controller_name' => 'SucreesController',
+            'recettes' => $recettes,
+
+        ]);
     }
+
+    /**
+     * @Route("/recettes sucrées/{id}", name="sucrees_show")
+     * @param RecetteRepository $repo
+     * @param $id
+     * @return Response
+     */
+
+    public function show(RecetteRepository $repo, $id)
+    {
+
+        $recette = $repo->find($id);
+        return $this->render('sucrees/show.html.twig', [
+            'recette' => $recette
+        ]);
+    }
+
 }
+
