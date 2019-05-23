30e621ee7d9c0dae94a3d8c99b0a07cb6acbdbee
fatima <fatima.elmajdoub@gmail.com>
2019-05-23 17:34:41 +0200

    premiere version de formulaire contact et envoie de mail par swift mailer et devmail n'est pas fini

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
+use App\Notification\ContactNotification;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,11 +15,38 @@ class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
+     * @param Request $request
      * @return Response
      */
 
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('pages/contact.html.twig');
+        $form = $this->createForm(ContactType::class);
+        return $this->render('pages/contact.html.twig', [
+            'form' =>$form->createView()
+        ]);
     }
+
+    /**
+     * @param Contact $contact
+     * @param Request $request
+     * @param ContactNotification $notification
+     * @return Response
+     */
+    public function show(Contact $contact, Request $request, ContactNotification $notification): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+        return $this->render('contact/show', [
+            'form' => $form->createView()
+        ]);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $notification->notify($contact);
+            $this->addFlash('succes', 'Votre email a bien été envoyé');
+            return $this->redirectToRoute('/');
+        }
+    }
+
 }
